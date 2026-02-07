79bb7e51b6339ee73a49f75cf98d259c8a3ca8f7
irfan <irfancsrrsimt@gmail.com>
2026-02-08 00:26:46 +0530

    export default app

--- server.js
+++ server.js
@@ -35,8 +35,10 @@ app.get("/",(req,res)=>{
         res.send("API working")
 })
 
-app.listen(port,()=>{
-    console.log(`Server started on http://localhost:${port}`)
-})
+export default app;
+
+// app.listen(port,()=>{
+//     console.log(`Server started on http://localhost:${port}`)
+// })
 
 //mongodb+srv://dulanjalisenarathna93:E2JUb0zfaT2FVp8D@cluster0.exkxkun.mongodb.net/?
